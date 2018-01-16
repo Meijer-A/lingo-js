7ddb5970e4236c4a3478077be0ba6508712b433b
Ayyoub Meijer <99032976@mydavinci.nl>
2018-01-16 12:04:04 +0100

    resultaat

--- game.js
+++ game.js
@@ -1,1 +1,39 @@
+var answer = words[Math.floor(Math.random()*words.length)];
+var answerArray = answer.split('');
+var typedWord = "";
+var rowCounter = 0;
+document.getElementById('letter0_0').value = answer[0];
+console.log(answer);
 
+var checkBtn = document.getElementById('check');
+
+checkBtn.setAttribute("onclick", "check();");
+
+function check(){
+  getTypedWord();
+  if (typedWord === answer) {
+    alert('gewonnen');
+  }
+
+  for(var i = 0; i < answer.length; i++){
+    if (typedWord[i] === answer[i]) {
+      document.getElementById('letter'+rowCounter+'_' + i).style.backgroundColor = 'red';
+    } else {
+      for (var j = 0; j < answer.length; j++) {
+        if (i!=j){
+          if (typedWord[j] === answer[i]){
+            document.getElementById('letter'+rowCounter+'_'+ j).style.backgroundColor = 'yellow';
+          }
+        }
+      }
+    }
+  }
+  rowCounter++;
+}
+function getTypedWord(){
+ typedWord = "";
+ for (var i = 0; i < answer.length; i++) {
+   typedWord += document.getElementById('letter'+rowCounter+'_' + i).value;
+ }
+
+}
